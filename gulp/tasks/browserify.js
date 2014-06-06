547cf3c655e6955fc3a7c5e464125fbda2624ff4
Ross Donaldson <gastove@gmail.com>
2014-06-05 18:24:01 -0700

    Add reactify package to package.json to support JSX.
    Add reactify to browserify task. It... works so far?

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,10 +1,12 @@
 var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
+var reactify = require('reactify');
 
 gulp.task('browserify', function() {
     return browserify('./js/app.js')
         .bundle()
+        .pipe(reactify())
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('./build/'));
 });
