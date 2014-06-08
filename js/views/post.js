df5b56404da84dae206318041d00d29f162ddc95
Ross Donaldson <gastove@gmail.com>
2014-06-08 00:04:42 -0700

    Get Markdown working! Fuck yes!

--- js/views/post.js
+++ js/views/post.js
@@ -7,36 +7,19 @@ var $ = require('jquery');
 var _ = require('underscore');
 var post = require('../models/post');
 var React = require('react');
+var Marked = require('marked');
 
 Backbone.$ = $;
 
 var PostView = module.exports = React.createClass({
     render: function() {
+        var md = Marked(this.props.post.get("body"));
         return (
                 <div className = "postViepw">
                 <h1>I pretty much dont even</h1>
                 <h1>{this.props.post.get("id")}</h1>
-                <div>{this.props.post.get("body")}</div>
+                <span dangerouslySetInnerHTML={{__html: md}} />
                 </div>
         );
     }
 });
-
-
-// React.renderComponent(
-//     <h1> Hello World!</h1>,
-//     Document.getById('example')
-// );
-
-// var PostView = module.exports = Backbone.View.extend ({
-//     tagName: 'div',
-//     className: 'postsContainer',
-//     template: _.template( $( '#postTemplate' ).html() ),
-
-//     render: function() {
-//         this.$el.html( this.template( this.model.toJSON() ) );
-
-//         return this;
-//     }
-
-// });
