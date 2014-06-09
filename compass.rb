20ce53584281941103f23ffccb1bc7a51062b8ea
Ross Donaldson <gastove@gmail.com>
2014-06-08 22:42:56 -0700

    Get the compass task working and correctly configured.

--- compass.rb
+++ compass.rb
@@ -1,7 +1,7 @@
 preferred_syntax = :scss
 http_path = '/'
-css_dir = 'build'
-sass_dir = 'js/scss'
+css_dir = 'build/stylesheets'
+sass_dir = 'js/sass'
 images_dir = 'build/images'
 fonts_dir = 'build/fonts'
 relative_assets = true
--- gulp/tasks/compass.js
+++ gulp/tasks/compass.js
@@ -4,11 +4,11 @@ var notify = require('gulp-notify');
 var compass = require('gulp-compass');
 
 gulp.task('compass', function() {
-    return gulp.src('./js/scss/*.scss')
+    return gulp.src('./js/sass/*.scss')
         .pipe(compass({
-            config_file: compass.rb,
-            css: 'build',
-            sass: 'src/scss'
+            config_file: './compass.rb',
+            css: './build/stylesheets',
+            sass: './js/sass'
         }))
         .on('error', handleErrors);
 });
