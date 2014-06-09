66d7d04ce2e231505d65bf81884a7893a5f856cd
Ross Donaldson <gastove@gmail.com>
2014-06-08 17:29:33 -0700

    Require BackboneMixin

--- js/app.js
+++ js/app.js
@@ -6,25 +6,9 @@ var _ = require('underscore');
 var PostsView = require('./views/postsView');
 var Posts = require('./collections/posts');
 var React = require('react');
+var BackboneMixin = require('./backbone_mixin');
 
 Backbone.$ = $;
-
-var BackboneMixin = {
-    componentDidMount: function() {
-        // Whenever there may be a change in the Backbone data, trigger a reconcile.
-        this.getBackboneModels().forEach(function(model) {
-            model.on('add change remove', this.forceUpdate.bind(this, null), this);
-        }, this);
-    },
-
-    componentWillUnmount: function() {
-        // Ensure that we clean up any dangling references when the component is
-        // destroyed.
-        this.getBackboneModels().forEach(function(model) {
-            model.off(null, null, this);
-        }, this);
-    }
-};
 
 var app = React.createClass({
     mixins: [BackboneMixin],
