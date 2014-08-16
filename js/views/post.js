f671470c09817b159dbe37edf75ade72945b60c1
Ross Donaldson <gastove@gmail.com>
2014-08-15 23:03:30 -0700

    Tweak headers.

--- js/views/post.js
+++ js/views/post.js
@@ -16,9 +16,9 @@ var PostView = module.exports = React.createClass({
         var md = Marked(this.props.post.get("body"));
         return (
                 <div>
-                <h1>I pretty much dont even</h1>
-                <h1>{this.props.post.get("title")}</h1>
-                <h2>{this.props.post.get("created_on")}</h2>
+                <h2>I pretty much dont even</h2>
+                <h2>{this.props.post.get("title")}</h2>
+                <h3>{this.props.post.get("created_on")}</h3>
                 <span dangerouslySetInnerHTML={{__html: md}} />
                 </div>
         );
