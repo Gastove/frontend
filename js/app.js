be4b8412e6bc1bf179f9ce388a32f6af3eee4598
Ross Donaldson <gastove@gmail.com>
2014-08-15 23:03:46 -0700

    Fiddle around the order of dependencies; add a gravatar URL

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 /**@jsx React.DOM */
 
 var Backbone = require('backbone');
-var PostsView = require('./views/postsView');
-var Posts = require('./collections/posts');
 var React = require('react');
 var BackboneMixin = require('./backbone_mixin');
+
+var Posts = require('./collections/posts');
+var PostsView = require('./views/postsView');
+var Sidebar = require('./views/sidebar');
+
+var avatarUrl = "http://www.gravatar.com/avatar/a942cea13e537bb0ea754b6d216c3377?size=160"
 
 var app = React.createClass({
     mixins: [BackboneMixin],
