c9b75c91348b88dbf7ac7e05ad6993b63cd9cd84
Ross Donaldson <gastove@gmail.com>
2014-08-17 09:07:58 -0700

    Add a header view.

---
+++ js/views/header.jsx
@@ -0,0 +1,11 @@
+/**@jsx React.DOM */
+
+var React = require('react');
+
+var Header = module.exports = React.createClass({
+    render: function() {
+        return (
+                <div className="bananagram">Ross Donaldson's Interblag Wobthing!</div>
+        );
+    }
+});
