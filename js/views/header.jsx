40d8b79393dd1ff6f5ed54b0afb358dfe34516c7
Ross Donaldson <gastove@gmail.com>
2014-08-17 10:30:15 -0700

    Formatting.

--- js/views/header.jsx
+++ js/views/header.jsx
@@ -5,7 +5,9 @@ var React = require('react');
 var Header = module.exports = React.createClass({
     render: function() {
         return (
-                <div className="bananagram">Ross Donaldson's Interblag Wobthing!</div>
+            <div className="bananagram">
+              Ross Donaldson's Interblag Wobthing!
+            </div>
         );
     }
 });
