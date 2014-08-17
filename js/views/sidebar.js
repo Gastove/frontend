99cb5d4961520deacdca9387b9b85aed40a13f31
Ross Donaldson <gastove@gmail.com>
2014-08-17 09:07:52 -0700

    Give sidebar a class.

--- js/views/sidebar.js
+++ js/views/sidebar.js
@@ -5,7 +5,7 @@ var React = require('react');
 var Sidebar = module.exports = React.createClass({
     render: function() {
         return (
-                <div>
+                <div className="side-bar">
                     <img src={this.props.avatarUrl} href="www.gastove.com"/>
                 </div>
         );
