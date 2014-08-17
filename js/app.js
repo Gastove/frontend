06c8d9fc854c242ff750718aae8889bd31dd9637
Ross Donaldson <gastove@gmail.com>
2014-08-16 22:36:18 -0700

    Add the Header class. Also: formatting (trying out web-mode)

--- js/app.js
+++ js/app.js
@@ -7,26 +7,31 @@ var BackboneMixin = require('./backbone_mixin');
 var Posts = require('./collections/posts');
 var PostsView = require('./views/postsView');
 var Sidebar = require('./views/sidebar');
+var Header = require('./views/header');
 
-var avatarUrl = "http://www.gravatar.com/avatar/a942cea13e537bb0ea754b6d216c3377?size=160"
+var avatarUrl = "http://www.gravatar.com/avatar/a942cea13e537bb0ea754b6d216c3377?size=160";
 
 var app = React.createClass({
-    mixins: [BackboneMixin],
+  mixins: [BackboneMixin],
 
-    componentDidMount: function() {
-        this.props.posts.fetch();
-    },
+  componentDidMount: function() {
+    this.props.posts.fetch();
+  },
 
-    getBackboneModels: function() {
-        return [this.props.posts];
-    },
+  getBackboneModels: function() {
+    return [this.props.posts];
+  },
 
-    render: function () {
-        return (
-                <PostsView posts={this.props.posts} />
-        );
-    }
+  render: function () {
+    return (
+      <div className="app">
+        <Header />
+        <Sidebar avatarUrl={avatarUrl} />
+        <PostsView posts={this.props.posts} />
+      </div>
+    );
+  }
 });
 
 React.renderComponent(<app posts={new Posts()} />,
-                      document.getElementById('posts'));
+                      document.getElementById('web-app'));
