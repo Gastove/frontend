86dd84320503abe4edd1e5c64de1affccbc854cd
Ross Donaldson <gastove@gmail.com>
2014-08-20 01:08:52 -0700

    Move the Avatar back out of the header :-P

--- js/views/header.jsx
+++ js/views/header.jsx
@@ -6,7 +6,6 @@ var Header = module.exports = React.createClass({
     render: function() {
         return (
             <div className="header">
-              <img src={this.props.avatarUrl} href="www.gastove.com"/>
               <div className="text">
                 <h1>
                   Ross Donaldson's Interblag Wobthing!
