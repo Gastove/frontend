524325b7d1ab92bbd858fa78bca32b85ef786575
Ross Donaldson <gastove@gmail.com>
2014-08-20 01:10:48 -0700

    Formatting; remove extraneous underscore require

--- js/collections/posts.js
+++ js/collections/posts.js
@@ -2,6 +2,9 @@ var Backbone = require('backbone');
 var Post = require('../models/post');
 
 var Posts = module.exports = Backbone.Collection.extend ({
+
     model: Post,
-    url: 'http://localhost:5000/api/home/'
+
+    url: 'http://localhost:5000/api/home/',
+
 });
