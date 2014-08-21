5c7dcc2fef35a6f210f72c9c423700ede26b65f1
Ross Donaldson <gastove@gmail.com>
2014-08-20 19:27:02 -0700

    Move "Interblag Wobthing" into its own element for styling

--- js/views/header.jsx
+++ js/views/header.jsx
@@ -8,7 +8,7 @@ var Header = module.exports = React.createClass({
             <div className="header">
               <div className="text">
                 <h1>
-                  Ross Donaldson's Interblag Wobthing!
+                  Ross Donaldson's <p>Interblag Wobthing!</p>
                 </h1>
               </div>
             </div>
