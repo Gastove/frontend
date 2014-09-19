f98eb6e9f5416f527a5bb98555a51fefd52b5ad6
Ross Donaldson <gastove@gmail.com>
2014-09-19 10:13:11 -0700

    Change mind -- start moving back to a single file (git.js) for all the git stuffz.

--- gulp/tasks/git-add.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var git = require('gulp-git');
-
-gulp.task('git-add', function(){
-    return gulp.src('buildDir')
-        .pipe(git.add());
-});
---
+++ gulp/tasks/git.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+
+var add = function(toAdd){
+    return gulp.src(toAdd)
+        .pipe(git.add());
+};
+
+var commit = function(msg){
+    return gulp.src('buildDir/*')
+        .pipe(git.commit(msg));
+};
+
+var push = function(dest) {
+    git.push(dest, 'master', function(err) {
+        if (err) throw err;
+    });
+};
+
+var changeBranch = function(dest) {};
+
+gulp.task('commit-build', function() {
+    return gulp.src(ADDS)
+        .pipe(add(ADDS))
+    // TODO: Need a timestamp and a better message.
+        .pipe(commit("Committing build"));
+});
+
+gulp.task('push-to-origin', function(){
+    return push('origin');
+});
+
+gulp.task('push-to-heroku', function(){
+    return push('herokue');
+});
+
+// gulp.task('add', function(){
+//     return gulp.src('buildDir')
+//     .pipe(git.add());
+// });
+
+// gulp.task('commit', function(){
+//     var msg = 'WRITE ME';
+
+//     return gulp.src('buildDir/*')
+//     .pipe(git.commit(msg));
+
+// });
+
+// gulp.task('push-origin-master', function() {
+//     git.push('origin', 'master', function(err) {
+//         if (err) throw err;
+//     });
+// });
+
+// gulp.task('push-heroku-master', function() {
+//     git.push('origin', 'heroku', function(err) {
+//         if (err) throw err;
+//     });
+// });
