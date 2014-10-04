923fefb98dc77cd8af947cf3b552b281bbd1c125
Ross Donaldson <gastove@gmail.com>
2014-10-04 15:46:40 -0700

    Remove ruby files from git cache

--- compass.rb
+++
@@ -1,8 +0,0 @@
-preferred_syntax = :scss
-http_path = '/'
-css_dir = 'build/stylesheets'
-sass_dir = 'js/sass'
-images_dir = 'build/images'
-fonts_dir = 'build/fonts'
-relative_assets = true
-line_comments = true
