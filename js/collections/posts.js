c8610ae95169c6d00b39020a49e77b801b0fe1fc
Ross Donaldson <gastove@gmail.com>
2014-10-05 16:06:56 -0700

    Add a todo comment

--- js/collections/posts.js
+++ js/collections/posts.js
@@ -5,6 +5,7 @@ var Posts = module.exports = Backbone.Collection.extend ({
 
     model: Post,
 
+    // TODO: Control this with an env variable of some sort.
     url: 'http://thermador.herokuapp.com/api/page',
 
 });
