13a2bb98e2922d2fb4ce6fbb46b1b84a39159902
Ross Donaldson <gastove@gmail.com>
2016-08-18 20:07:27 -0700

    Update to handle routing

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var path = require ('path');
 var server = express();
 
 server.set('port', (process.env.PORT || 5000));
 server.use(express.static(__dirname + '/public'));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+server.get('*', function (request, response){
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
 
 server.listen(server.get('port'));
